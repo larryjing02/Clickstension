4b31207aa6c89820395981e536226e1d6af54ddf
jingla <jingla@cs.washington.edu>
2021-12-27 15:12:28 -0800

    Some style modifications

--- popup.js
+++ popup.js
@@ -8,7 +8,16 @@ function testFunc() {
     });
 }
 
+function convertTime(s) {
+    var hours   = Math.floor(s / 3600)
+    var minutes = Math.floor(s / 60) % 60
+    var seconds = s % 60
 
+    return [hours,minutes,seconds]
+        .map(v => v < 10 ? "0" + v : v)
+        .filter((v,i) => v !== "00" || i > 0)
+        .join(":")
+}
 
 window.onload = function() {
     //document.getElementById("clickactivity").addEventListener("click", testFunc);
@@ -21,12 +30,23 @@ window.onload = function() {
 
         let table = document.getElementById("timeList");
         
+        // // Set table header
+        // let header = document.createElement("tr");
+        // let key = document.createElement("td");
+        // key.appendChild(document.createTextNode("Site Domain"));
+        // let val = document.createElement("td");
+        // val.appendChild(document.createTextNode("Time Spent"));
+        // header.appendChild(key);
+        // header.appendChild(val);
+        // table.appendChild(header);
+
+        // Append all data to table
         sortedData.forEach((item)=>{
             let row = document.createElement("tr");
             let key = document.createElement("td");
             key.appendChild(document.createTextNode(item));
             let val = document.createElement("td");
-            val.appendChild(document.createTextNode(timeMap[item]));
+            val.appendChild(document.createTextNode(convertTime(timeMap[item])));
 
             row.appendChild(key);
             row.appendChild(val);
