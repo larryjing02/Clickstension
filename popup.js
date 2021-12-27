66017e1c57ce7e1126e8f98b202baeed3a314b7a
jingla <jingla@cs.washington.edu>
2021-12-26 23:29:00 -0800

    Added storage incrementer

--- popup.js
+++ popup.js
@@ -1,16 +1,15 @@
 function testFunc() {
-    console.log("testFunc HERE")
-
-    chrome.tabs.get(activeInfo.tabId, function(tab){
-       console.log(tab.url);
-    });
-
-    chrome.storage.local.get(["count"], (result) => {
-        const count = result.count ? result.count++ : 1;
-        chrome.storage.local.set({count});
+    console.log("Function: testFunc");
+    
+    chrome.storage.local.get("counter", (result) => {
+        const count = result.counter != null ? result.counter + 1 : 1;
+        chrome.storage.local.set({"counter":count}, function(){});
         console.log(count);
     });
 }
 
-document.getElementById("clickMe").addEventListener("click", testFunc);
 
+
+window.onload = function() {
+    document.getElementById("clickactivity").addEventListener("click", testFunc);
+};
