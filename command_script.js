2e3a20116152ab873e906f1f5023392ff90fc491
jingla <jingla@cs.washington.edu>
2021-12-27 14:27:26 -0800

    Remove unnecessary files

--- command_script.js
+++
@@ -1,3 +0,0 @@
-const elements = document.getElementsByClassName("nav-a nav-a-2 nav-progressive-attribute")
-const elementToClick = elements[Math.floor(Math.random() * elements.length)]
-elementToClick.click()
