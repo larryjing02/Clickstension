4cd5ba6881afedf05c3a3f38832c4915777d38e6
jingla <jingla@cs.washington.edu>
2021-12-27 14:26:25 -0800

    Displays ranked seconds table

--- popup.js
+++ popup.js
@@ -11,19 +11,27 @@ function testFunc() {
 
 
 window.onload = function() {
-    document.getElementById("clickactivity").addEventListener("click", testFunc);
+    //document.getElementById("clickactivity").addEventListener("click", testFunc);
     
     // Get time attributes from storage
     chrome.storage.local.get("timeMap", (result) => {
-        let timeMap = result.timeMap != null ? result.timeMap : {"key":"val"};
-        let data = Object.keys(timeMap);
-    
-        let list = document.getElementById("timeList");
+        let timeMap = result.timeMap != null ? result.timeMap : {"Website Domain":"0"};  
+
+        let sortedData = Object.keys(timeMap).sort(function(a,b){return timeMap[b]-timeMap[a]});
+
+        let table = document.getElementById("timeList");
         
-        data.forEach((item)=>{
-            let li = document.createElement("li");
-            li.innerText = item;
-            list.appendChild(li);
+        sortedData.forEach((item)=>{
+            let row = document.createElement("tr");
+            let key = document.createElement("td");
+            key.appendChild(document.createTextNode(item));
+            let val = document.createElement("td");
+            val.appendChild(document.createTextNode(timeMap[item]));
+
+            row.appendChild(key);
+            row.appendChild(val);
+
+            table.appendChild(row);
         });
     });
 };
