23322afa172a72499c5941168aa4df50e0e327f2
jingla <jingla@cs.washington.edu>
2021-12-27 14:09:40 -0800

    Site Ranked List added

--- background.js
+++ background.js
@@ -13,7 +13,7 @@ async function getCurrentDomain() {
 
 // Gets previous domain
 async function getSession() {
-    console.log("Getting prev session");
+    // console.log("Getting prev session");
     var p = new Promise(function(resolve, reject) {
         chrome.storage.local.get("curSession", (result) => {
         var site = result.curSession != null ? result.curSession : getCurrentDomain();
@@ -26,24 +26,25 @@ async function getSession() {
 
 // Gets elapsed time in seconds
 async function getElapsedTime() {
-    console.log("Getting start time");
+    //console.log("Getting start time");
     var p = new Promise(function(resolve, reject) {
         chrome.storage.local.get("timeA", (result) => {
-        var time = result.timeA != null ? result.timeA : performance.now();
-        console.log(time);
+        var time = result.timeA != null ? result.timeA : Math.floor(performance.now());
+        // console.log(time);
         resolve(time);
     })});
 
     const prevTime = await p;
+
     const elapsed = Math.floor((performance.now() - prevTime)/1000);
     return elapsed
 }
 
 // Sets value of domain, start time in storage
 function setSession(curDomain) {
-    console.log("Setting cur session")
+    // console.log("Setting cur session")
     chrome.storage.local.set({curSession:curDomain}, function(){});
-    chrome.storage.local.set({timeA:performance.now()}, function(){});
+    chrome.storage.local.set({timeA:Math.floor(performance.now())}, function(){});
 }
 
 // On tab activity, updates stored times if necessary
@@ -53,10 +54,17 @@ async function updateStorage() {
 
     // If cur != prev then we changed domains
     if (cur != prev) {
-        console.log("Domain change detected!");
+        console.log("Domain change detected: " + cur);
 
         var elapsed = await getElapsedTime();
-        console.log(elapsed);
+
+        // If elapsed time returns negative then new session just began
+        // Thus, is necessary to use current performance.now() as time factor
+        if (elapsed < 0) {
+            elapsed = Math.floor(performance.now()/1000);
+        }
+
+        console.log("Elapsed time for " + prev + ": " + elapsed);
 
         chrome.storage.local.get("timeMap", (result) => {
             var map = result.timeMap;
@@ -72,9 +80,11 @@ async function updateStorage() {
             chrome.storage.local.set({"timeMap":map}, function(){});
             console.log(map);
         });
-        setSession(cur);
+        if (cur != "error") {
+            setSession(cur);
+        }
     } else {
-        console.log("Ignoring");
+        console.log("Event ignored");
     }
 }
 
@@ -84,6 +94,11 @@ chrome.tabs.onUpdated.addListener(function(activeInfo) {
 });
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
+    console.log("Activation Detected");
+    updateStorage();
+});
+
+chrome.windows.onFocusChanged.addListener(function(activeInfo) {
     console.log("Activation Detected");
     updateStorage();
 });
--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 function testFunc() {
     console.log("Function: testFunc");
-    
+
     chrome.storage.local.get("counter", (result) => {
         const count = result.counter != null ? result.counter + 1 : 1;
         chrome.storage.local.set({"counter":count}, function(){});
@@ -12,4 +12,18 @@ function testFunc() {
 
 window.onload = function() {
     document.getElementById("clickactivity").addEventListener("click", testFunc);
+    
+    // Get time attributes from storage
+    chrome.storage.local.get("timeMap", (result) => {
+        let timeMap = result.timeMap != null ? result.timeMap : {"key":"val"};
+        let data = Object.keys(timeMap);
+    
+        let list = document.getElementById("timeList");
+        
+        data.forEach((item)=>{
+            let li = document.createElement("li");
+            li.innerText = item;
+            list.appendChild(li);
+        });
+    });
 };
