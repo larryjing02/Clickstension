1b9d59dfc8cadf490f5f3f204a16f72359d92748
jingla <jingla@cs.washington.edu>
2021-12-27 00:57:19 -0800

    Added second elapsed detection.

--- background.js
+++ background.js
@@ -1,47 +1,76 @@
+// Gets domain of currently active tab
 async function getCurrentDomain() {
     let queryOptions = { active: true, currentWindow: true };
     let [tab] = await chrome.tabs.query(queryOptions);
-    let url = new URL(tab.url);
-    // console.log(url.hostname);
-    return url.hostname;
+    if (tab != null) {
+        let url = new URL(tab.url);
+        return url.hostname;
+    } else {
+        console.log("Error");
+        return "error";
+    }
 }
 
-
+// Gets previous domain
 async function getSession() {
-    console.log("Getting cur session");
+    console.log("Getting prev session");
     var p = new Promise(function(resolve, reject) {
         chrome.storage.local.get("curSession", (result) => {
         var site = result.curSession != null ? result.curSession : getCurrentDomain();
-        console.log(site);
         resolve(site);
     })});
-    console.log("Preprom")
-    const prevDomain = await p;
-    console.log("Postprom")
-    console.log(prevDomain)
 
+    const prevDomain = await p;
     return prevDomain
 }
 
+// Gets elapsed time in seconds
+async function getElapsedTime() {
+    console.log("Getting start time");
+    var p = new Promise(function(resolve, reject) {
+        chrome.storage.local.get("timeA", (result) => {
+        var time = result.timeA != null ? result.timeA : performance.now();
+        console.log(time);
+        resolve(time);
+    })});
+
+    const prevTime = await p;
+    const elapsed = Math.floor((performance.now() - prevTime)/1000);
+    return elapsed
+}
+
+// Sets value of domain, start time in storage
 function setSession(curDomain) {
     console.log("Setting cur session")
     chrome.storage.local.set({curSession:curDomain}, function(){});
+    chrome.storage.local.set({timeA:performance.now()}, function(){});
 }
 
+// On tab activity, updates stored times if necessary
 async function updateStorage() {
     var prev = await getSession();
-    console.log("prev");
-    console.log(prev);
     var cur = await getCurrentDomain();
-    console.log("cur");
-    console.log(cur);
+
     // If cur != prev then we changed domains
     if (cur != prev) {
-        console.log("domain change detected!");
-        chrome.storage.local.get("counter", (result) => {
-            const count = result.counter != null ? result.counter + 1 : 1;
-            chrome.storage.local.set({"counter":count}, function(){});
-            console.log(count);
+        console.log("Domain change detected!");
+
+        var elapsed = await getElapsedTime();
+        console.log(elapsed);
+
+        chrome.storage.local.get("timeMap", (result) => {
+            var map = result.timeMap;
+            if (map != null) {
+                if (prev in map) {
+                    map[prev] += elapsed;
+                } else {
+                    map[prev] = elapsed;
+                }
+            } else {
+                map = {prev : elapsed};
+            };
+            chrome.storage.local.set({"timeMap":map}, function(){});
+            console.log(map);
         });
         setSession(cur);
     } else {
@@ -50,15 +79,15 @@ async function updateStorage() {
 }
 
 chrome.tabs.onUpdated.addListener(function(activeInfo) {
-    console.log("Updated");
+    console.log("Updated Detected");
     updateStorage();
 });
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-    console.log("Updated");
+    console.log("Activation Detected");
     updateStorage();
 });
 
-// add on activated, on created?
+// Modify functionality to account for when browser startup/shutdown, see details about active tab?
 
 
