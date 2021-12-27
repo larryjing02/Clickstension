16ba2ad3927111cea9fa8bf59e958c1d16c06f77
jingla <jingla@cs.washington.edu>
2021-12-27 00:25:46 -0800

    Updated event listeners

--- background.js
+++ background.js
@@ -1,27 +1,64 @@
 async function getCurrentDomain() {
     let queryOptions = { active: true, currentWindow: true };
     let [tab] = await chrome.tabs.query(queryOptions);
-    let url = new URL(tab.url)
-    console.log("Something happened")
-    console.log(url.hostname)
+    let url = new URL(tab.url);
+    // console.log(url.hostname);
     return url.hostname;
 }
 
 
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-    console.log("UPDATED HERE")
-    chrome.storage.local.get(["count"], (result) => {
-        const count = result.count ? result.count++ : 1;
-        chrome.storage.local.set({count});
-        console.log(count);
-    });
-    console.log("counter finished")
-    getCurrentDomain();
-    chrome.tabs.get(activeInfo.tabId, function(tab){
-       console.log(tab.url);
-    });
-    
+async function getSession() {
+    console.log("Getting cur session");
+    var p = new Promise(function(resolve, reject) {
+        chrome.storage.local.get("curSession", (result) => {
+        var site = result.curSession != null ? result.curSession : getCurrentDomain();
+        console.log(site);
+        resolve(site);
+    })});
+    console.log("Preprom")
+    const prevDomain = await p;
+    console.log("Postprom")
+    console.log(prevDomain)
+
+    return prevDomain
+}
+
+function setSession(curDomain) {
+    console.log("Setting cur session")
+    chrome.storage.local.set({curSession:curDomain}, function(){});
+}
+
+async function updateStorage() {
+    var prev = await getSession();
+    console.log("prev");
+    console.log(prev);
+    var cur = await getCurrentDomain();
+    console.log("cur");
+    console.log(cur);
+    // If cur != prev then we changed domains
+    if (cur != prev) {
+        console.log("domain change detected!");
+        chrome.storage.local.get("counter", (result) => {
+            const count = result.counter != null ? result.counter + 1 : 1;
+            chrome.storage.local.set({"counter":count}, function(){});
+            console.log(count);
+        });
+        setSession(cur);
+    } else {
+        console.log("Ignoring");
+    }
+}
+
+chrome.tabs.onUpdated.addListener(function(activeInfo) {
+    console.log("Updated");
+    updateStorage();
 });
 
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    console.log("Updated");
+    updateStorage();
+});
+
+// add on activated, on created?
 
 
